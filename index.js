fc10e38647a376db2582026bfc4c071c33e640d7
samkeen <skeen@mixpo.com>
2015-04-08 21:25:29 -0700

    using templating handlebars fixes #1

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
+
 server.connection({
-    host: 'localhost',
-    port: 8111
+    port: Number(process.argv[2] || 8111),
+    host: "localhost"
+});
+
+server.views({
+    engines: {
+        html: require("handlebars")
+    },
+    relativeTo: __dirname,
+    path: "views",
+    layoutPath: "./views/layouts",
+    layout: true,
+    partialsPath: "./views/partials",
+    helpersPath: "./views/helpers"
 });
 
 server.route({
     method: 'GET',
     path: '/hello',
     handler: function (request, reply) {
-        reply('hello world');
+        reply.view('index', { title: 'sharv', body: 'Item Body' });
     }
 });
 
