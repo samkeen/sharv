da9c0517d3a8b9ff252b724956b3723b8814b527
samkeen <skeen@mixpo.com>
2015-04-10 08:30:39 -0700

    added data storage  - going with rethinkdb. Been wanting to try it out  - added a uuid lib

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
+var uuid = require('node-uuid');
+var r = require('rethinkdb');
+
+var connection = null;
+r.connect( {host: 'localhost', port: 28015, db: 'sharv'}, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+});
 
 server.connection({
     port: Number(process.argv[2] || 8111),
@@ -23,7 +31,25 @@ server.route({
     method: 'GET',
     path: '/hello',
     handler: function (request, reply) {
-        reply.view('index', { title: 'sharv', body: 'Item Body' });
+        reply.view('index', {title: 'sharv', body: 'Item Body'});
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/har',
+    handler: function (request, reply) {
+
+        var harId = uuid.v4();
+
+        r.table('har').insert([
+            {har_id: harId, content: request.payload, headers: request.headers}
+        ]).run(connection, function(err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+        });
+
+        reply.view('index', {title: 'sharv', body: uuid.v1(), "payload": JSON.stringify(request.payload)});
     }
 });
 
